0deb8d6cc5cdf390387f49eb5ca4e935d97ed4da
Hernan Anskaitiene <hmanskaitiene@teco.com.ar>
2022-06-13 14:36:23 -0300

    Se quito la función async main

--- index.js
+++ index.js
@@ -3,25 +3,23 @@ const app = express()
 
 const Contenedor = require('./models/contenedor')
 const port = 8080
+const contenedor = new Contenedor();
 
-const main = async () => {
-    const contenedor = new Contenedor();
-    await contenedor.initFile();
-    app.get('/productos', async function (req, res) {
-        const productos = await contenedor.getAll();
-        res.send(productos);
-    })
-
-    app.get('/productoRandom', async function (req, res) {
-        const productos = await contenedor.getAll();
-        const random = Math.floor(Math.random() * productos.length) + 1;
-        const producto = await contenedor.getById(random);
-        res.send(JSON.stringify(producto));
-    })
-
-    app.listen(port, () => {
-        console.log(`Escuchando el puerto ${port}`)
-    })
+const getAll = async (req, res) => {
+    const productos = await contenedor.getAll();
+    res.status(200).json(productos);
 };
 
-main();
+const productoRandom = async (req, res) => {
+    const productos = await contenedor.getAll();
+    const random = Math.floor(Math.random() * productos.length) + 1;
+    const producto = await contenedor.getById(random);
+    res.status(200).json(producto);
+};
+
+app.get('/productos', getAll)
+app.get('/productoRandom', productoRandom)
+
+app.listen(port, () => {
+    console.log(`Escuchando el puerto ${port}`)
+})
